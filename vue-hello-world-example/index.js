6b5300f1317b95f81ba784224271e1917173dbb7
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-02-15 13:20:52 +0100

    Added example

---
+++ vue-hello-world-example/index.js
@@ -0,0 +1,17 @@
+
+//Vi skapar upp vår Vue-instans och kopplar till id:et app
+let App = new Vue({
+    el: '#app',
+    data: {
+        message: 'This is cool shit!',
+        name: ''
+    }
+});
+
+
+// let nameInput = '';
+
+// document.querySelector('#name-input').addEventListener('keyup', (event) => {
+//     nameInput = event.target.value;
+//     console.log(nameInput);
+// })
